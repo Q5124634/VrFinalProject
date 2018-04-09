848cd4502ed94266b0f497eab9ce0806e79d1b42
Q5124634 <thomasbarnabypass@hotmail.co.uk>
2018-04-09 17:06:19 +0100

    BackPack
    Getting objects from the bag

---
+++ Assets/Scripts/RWVR_SpecialObjectSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RWVR_SpecialObjectSpawner : RWVR_InteractionObject
+{
+
+    public GameObject SetPrefabAk47; // 1
+    public List<GameObject> randomPrefabs = new List<GameObject>(); // 2
+    private void SpawnObjectInHand(GameObject prefab, RWVR_InteractionController controller) // 1
+    {
+        GameObject spawnedObject = Instantiate(prefab, controller.snapColliderOrigin.position, controller.transform.rotation); // 2
+        controller.SwitchInteractionObjectTo(spawnedObject.GetComponent<RWVR_InteractionObject>()); // 3
+        OnTriggerWasReleased(controller); // 4
+    }
+    public override void OnTriggerWasPressed(RWVR_InteractionController controller) // 1
+    {
+        base.OnTriggerWasPressed(controller); // 2
+
+        //this is the bool to spawn a specific object if the object in the hand is a specific gun, this will be used to do magazines and other things.
+        if (RWVR_ControllerManager.Instance.Gun1)// 3
+        {
+            SpawnObjectInHand(SetPrefabAk47, controller);
+        }
+        else
+        {
+            SpawnObjectInHand(randomPrefabs[UnityEngine.Random.Range(0, randomPrefabs.Count)], controller);
+        }
+    }
+}
