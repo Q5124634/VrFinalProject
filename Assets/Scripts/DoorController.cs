75dd4a9ea5d265c14fb9ebf285c3c21025bde6f0
Q5124634 <thomasbarnabypass@hotmail.co.uk>
2018-04-17 18:32:28 +0100

    StartTimer and updated guns

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class DoorController : MonoBehaviour {
     public float speed = 0.005F;
     public GameObject Door;
     public GameObject DoorEnd;
+    public GameObject DoorStart;
     private float startTime;
     private float journeyLength;
     public bool DoorIsOpen;
     public bool StartTimer = false;
+    public Text TimerText;
+    private float TimerStart;
+    public float StartedTimer = 0;
+    private float NewTime;
+    public string SavedTime;
+    public GameObject EndButton;
 
 
     void Start()
@@ -18,6 +27,7 @@ public class DoorController : MonoBehaviour {
      float YValue = Door.transform.position.y;
      float ZValue = Door.transform.position.z;
 
+        
         startTime = Time.time;
         journeyLength = Vector3.Distance(Door.transform.position, DoorEnd.transform.position);
     }
@@ -27,17 +37,72 @@ public class DoorController : MonoBehaviour {
     void Update () {
         if(DoorIsOpen)
         {
-            float distCovered = (Time.time - startTime) * speed;
+            //TimerStart = Time.time;
+            float distCovered = (Time.deltaTime) * speed;
             float fracJourney = distCovered / journeyLength;
             Door.transform.position = Vector3.Lerp(Door.transform.position, DoorEnd.transform.position, fracJourney);
             StartTimer = true;
+             StartCoroutine(WaitOneSecond());
+           
         }
 
+        if (!DoorIsOpen)
+        {
+            //TimerStart = Time.time;
+            float distCovered = (Time.deltaTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            Door.transform.position = Vector3.Lerp(Door.transform.position, DoorStart.transform.position, fracJourney);
 
-	}
+        }
+
+        if (StartTimer == true)
+        {
+            StartTiming();
+        }
+
+        if (StartTimer == false)
+        {
+            EndTiming();
+            return;
+        }
+
+    }
+
+
+    void EndTiming()
+    {
+        SavedTime = TimerText.text;
+        TimerText.color = Color.cyan;
+    }
+
+    void StartTiming()
+    {
+        if (StartedTimer >= 0)
+        {
+            NewTime += Time.deltaTime;
+            float minuites = NewTime / 60;
+            float seconds = NewTime % 60;
+            float millSecs = (NewTime * 100) % 100;
+
+            TimerText.text = string.Format ("{0:00} : {1:00} : {2:00}", minuites, seconds , millSecs);
+        }
+        else
+        {
+
+        }
+
+        
+    }
+
     void OnMouseDown()
     {
         DoorIsOpen = true;
+    }
+
+    IEnumerator WaitOneSecond()
+    {
+        yield return new WaitForSeconds(5);
+        DoorIsOpen = false;
     }
 
 }
